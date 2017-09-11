35d5c85c5ad6bda806204251e3c7df4d2c4632ae
jaebradley <jae.b.bradley@gmail.com>
2017-09-11 17:41:45 -0500

    update yo sending implementation

--- scripts/yo.py
+++ scripts/yo.py
@@ -1,7 +1,9 @@
 import click
 import yo_client
+from requests.exceptions import HTTPError
+import emoji
 
-from src.account_details import get_account_details
+from src.data import get_account_details
 
 
 @click.group()
@@ -28,13 +30,25 @@ def set_api_key(api_key):
 @click.command()
 @click.option('--to', type=click.STRING, prompt='Send to whom?')
 @click.option('--message', type=click.STRING, prompt='What is your message?')
-def send(to, message):
+@click.option('--link', type=click.STRING)
+def send(to, message, link):
     details = get_account_details()
     if details.api_key:
         client = yo_client.YoClient(details.api_key)
-        response = client.send_yo(username=to, text=message)
-        print(response)
-
+        try:
+            response = client.send_yo(username=to, text=message, link=link)
+            recipient = response['recipient']['username']
+            yo_id = response['yo_id']
+            print(emoji.emojize(':+1: Sent Yo #{yo_id} to {recipient}! :+1:'.format(yo_id=yo_id, recipient=recipient),
+                                use_aliases=True))
+        except HTTPError:
+            print(emoji.emojize(':frowning: :confused: :anguished: :sweat:  Looks like there was a problem with the recipient, message, or link! :frowning: :confused: :anguished: :sweat:',
+                                use_aliases=True))
+        except BaseException as e:
+            if hasattr(e, 'message'):
+                print(e.message)
+            else:
+                print(e)
 
 yo.add_command(set_username)
 yo.add_command(set_api_key)
